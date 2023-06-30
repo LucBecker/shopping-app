9cc30a29d9594052f05138e4a0bf9b73d481949d
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-06-30 15:51:13 -0300

    Adding Filter Products

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -10,6 +10,8 @@ import { CartService } from 'src/app/service/cart.service';
 export class ProductsComponent implements OnInit {
 
   public productList: any;
+  public filterCategory : any
+  searchKey:string ="";
 
   constructor(
     private api: ApiService,
@@ -18,17 +20,33 @@ export class ProductsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.api.getProduct()
-    .subscribe(response=>{
-      this.productList = response;
-
+    this.api.getProduct().subscribe(res=>{
+      this.productList = res;
+      this.filterCategory = res;
       this.productList.forEach((a:any) => {
-        Object.assign(a, {quantity:1, total:a.price});
+        if(a.category ==="women's clothing" || a.category ==="men's clothing"){
+          a.category ="fashion"
+        }
+        Object.assign(a,{quantity:1,total:a.price});
       });
+      console.log(this.productList)
+    });
+
+    this.cartService.search.subscribe((val:any)=>{
+      this.searchKey = val;
     })
   }
 
   addToCart(item : any){
     this.cartService.addtoCart(item);
+  }
+
+  filter(category:string){
+    this.filterCategory = this.productList
+    .filter((a:any)=>{
+      if(a.category == category || category==''){
+        return a;
+      }
+    })
   }
 }
